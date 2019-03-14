2bb5c4d700d07706eac9b7ad0b02272ff68e9f94
brunodamasco <brunodamasco1@gmail.com>
2019-03-14 12:43:42 -0300

    inicio do projeto

---
+++ src/java/entidade/Produto.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Dell
+ */
+public class Produto implements Serializable{
+        
+    private Integer id;
+    private String nome;
+    private int quantidade;
+    private double precoCompra;
+    private double precoVenda;
+    private String descricao;
+    private Date cadastro;
+
+    public Produto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(Date cadastro) {
+        this.cadastro = cadastro;
+    }
+        
+    
+}
